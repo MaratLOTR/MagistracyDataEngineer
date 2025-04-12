6d6a4625a409b05465097700c0e76954fd61a38a
MaratLOTR <maratabdrahmanov@yandex.ru>
2025-04-13 02:22:39 +0500

    Удален код для моделей

--- schedule.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.post(
-    "https://isu.uust.ru/module/schedule/schedule_2024_script.php",
-    data={"funct": "group", "group_id": 8289, "week": 32}
-)
-print(response.text)
